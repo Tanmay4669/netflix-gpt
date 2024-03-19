070dfbecbaf70b9594655cfd4d4841f22086be08
Tanmay4669 <tanmayvaidya4669@gmail.com>
2024-03-20 01:09:57 +0530

    Added Memoization for Optimizing API Calls

--- src/hooks/usePopularMovies.js
+++ src/hooks/usePopularMovies.js
@@ -1,11 +1,13 @@
 import React, { useEffect } from "react";
 import { API_OPTIONS } from "../utils/constants";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addPopularMovies } from "../utils/moviesSlice";
 
 const usePopularMovies = () => {
   //Fetch data from TDMB website and update store
   const dispatch = useDispatch();
+
+  const popularMovies = useSelector((store) => store.movies?.popularMovies);
 
   const getPopularMovies = async () => {
     const data = await fetch(
@@ -19,7 +21,7 @@ const usePopularMovies = () => {
   };
 
   useEffect(() => {
-    getPopularMovies();
+    !popularMovies && getPopularMovies();
   }, []);
 };
 
